3bb744c0b1cbd8a37c09c3981911d457e2ecca0f
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2024-06-07 19:08:46 +0600

    working on setup the project structure

---
+++ servers/apps/users/src/dto/user.dto.ts
@@ -0,0 +1,33 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { Field, InputType } from '@nestjs/graphql';
+
+@InputType()
+export class RegisterDto {
+  @Field()
+  @IsNotEmpty({ message: 'Name is Required' })
+  @IsString({ message: 'Name must be a one string' })
+  name: string;
+
+  @Field()
+  @IsNotEmpty({ message: 'Password is Required' })
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  password: string;
+
+  @Field()
+  @IsNotEmpty({ message: 'Email is Required' })
+  @IsEmail({ message: 'Email must be a valid email address' })
+  email: string;
+}
+
+@InputType()
+export class LoginDto {
+  @Field()
+  @IsNotEmpty({ message: 'Email is Required' })
+  @IsEmail({ message: 'Email must be a valid email address' })
+  email: string;
+
+  @Field()
+  @IsNotEmpty({ message: 'Password is Required' })
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  password: string;
+}

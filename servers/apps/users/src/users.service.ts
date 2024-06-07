3a049e1a2ac87a6fcac622b25080ac2291c532fe
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2024-06-07 19:37:22 +0600

    started again the userservice code

--- servers/apps/users/src/users.service.ts
+++ servers/apps/users/src/users.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { LoginDto, RegisterDto } from './dto/user.dto';
+import { Response } from 'express';
 
 @Injectable()
 export class UsersService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  //   register user service
+
+  async register(registerDto: RegisterDto) {
+    const { name, email, password } = registerDto;
+
+    return { name, email, password };
+  }
+
+  //   login user service
+  async login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+    return { email, password };
   }
 }
